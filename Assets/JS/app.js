25195903162527d03d184ac1c904bb184e349ea4
mathiasmundt99 <mathiasbeck99@gmail.com>
2024-04-16 13:29:37 +0200

    oprettet functions

---
+++ Assets/JS/app.js
@@ -0,0 +1,41 @@
+// Dittes URL
+const baseUrl = 'https://api.xn--dittebrgesen-0jb.dk/wp-json/wp/v2/';
+const containerEl = document.querySelector('.container'); 
+const recipeID = 3;
+
+
+
+function getAllPosts(){
+fetch(baseUrl + 'posts')
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+    data.forEach((post) => renderPost(post))     
+    })
+.catch((err) => console.log('Error ', err));
+}
+
+// getAllPosts();
+
+function getPostsByCat(id){
+fetch(baseUrl +'posts?categories=' + id)
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+    data.forEach((post) => renderPost(post))     
+    })
+.catch((err) => console.log('Error ', err));
+}
+
+getPostsByCat(recipeID);
+
+function renderPost(post){
+    console.log('post', post);
+    containerEl.innerHTML += `
+    <article>
+        <h4>${post.title.rendered}</h4>
+        <p class="excerpt">${post.excerpt.rendered}</p>
+        <p class="description">${post.content.rendered}</p>
+    </article>
+    `;
+}
