5c3068e962bf1c878f082be7b3f13b40ce62f1d0
mathiasmundt99 <mathiasbeck99@gmail.com>
2024-04-25 09:23:51 +0200

    app.js updated

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,34 +1,63 @@
-const baseUrl = 'https://mundt.gg/wp-json/wp/v2/';
-const containerEl = document.querySelector('.container'); 
-const recipeID = 3;
+const baseUrl ='https://mundt.gg/wp-json/wp/v2/';
+
+const recipeId =3;
+
+const fastCooktimeId = 8;
+const mediumCooktimeId = 9;
+const slowCooktimeId = 10;
+
+const containerEL = document.querySelector('.container');
 
 
+// Måske en if statement på undersiderne
+function getToken(){
+    const loginInfo = {
+        username: 'apiAdmin',
+        password: 'AvoG j6W3 Fe8B VHBH lb43 ZuQU'
+    }
 
-function getAllPosts(){
-fetch(baseUrl + 'posts')
-.then((res) => res.json())
-.then((data) => {
-    console.log(data);
-    data.forEach((post) => renderPost(post))     
+    return fetch('https://mundt.gg/wp-json/jwt-auth/v1/token', {
+        method: 'POST', 
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(loginInfo)
     })
-.catch((err) => console.log('Error ', err));
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        sessionStorage.setItem('myToken', data.data.token)
+    })
+    .catch(err => console.log('Error: ', err))
 }
 
-getAllPosts();
+getToken().then(() => getPrivateRecipes());
 
-function getPostsByCat(id){
-fetch(baseUrl +'posts?categories=' + id)
-.then((res) => res.json())
-.then((data) => {
-    console.log(data);
-    data.forEach((post) => renderPost(post))     
+function getPrivateRecipes(){
+    fetch(baseUrl + `posts?status=private&categegories=${recipeId}`, {
+        headers: {
+            Authorization: "Bearer " + sessionStorage.getItem('myToken')
+        }
     })
-.catch((err) => console.log('Error ', err));
+    .then(res => res.json())
+    .then(recipes => {
+        console.log(recipes);
+    })
+    .catch(err => console.log('Error: ', err))
 }
 
-getPostsByCat(recipeID);
+// getPrivateRecipes();
 
-function renderPost(post){
+function getPostByTaxonomies(cooktime, diet){
+    fetch(baseUrl + `posts?status`)
+}
+
+//getPostByTax.then.render()
+
+
+
+
+function renderRecipe(post){
     console.log('post', post);
     containerEl.innerHTML += `
     <article>
